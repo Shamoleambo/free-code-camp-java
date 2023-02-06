ae5a8472975a562e440c5a27abb3094c8597e35c
Tiago Gomes <tidgomes@gmail.com>
2023-02-06 13:01:55 -0300

    create a for loop

--- tutorial/src/tutorial/Main.java
+++ tutorial/src/tutorial/Main.java
@@ -3,22 +3,10 @@ package tutorial;
 public class Main {
 
 	public static void main(String[] args) {
-		String[] newArr = new String[5];
-		newArr[0] = "mano";
-		newArr[1] = "truta";
-		newArr[2] = "tiu";
-		newArr[3] = "fulano";
-		newArr[4] = "beltrano";
-
-		int[] nums = { 1, 2, 3, 5, 8 };
-		double[] doubleArr = { 1, 2, 3, 4, 10 };
-
-		int num = nums[4];
-		double doub = doubleArr[4];
-
-		System.out.println(num);
-		System.out.println(doub);
-
+		int[] arr = { 1, 1, 2, 3, 5, 8, 13 };
+		for (int i = 0; i < arr.length; i++) {
+			System.out.printf("Position %d: %d%n", i, arr[i]);
+		}
 	}
 
 }
