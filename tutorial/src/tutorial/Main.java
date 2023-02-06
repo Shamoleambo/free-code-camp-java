bec01ffe90d8ab900987f67fe78c9bb05c2a93ff
Tiago Gomes <tidgomes@gmail.com>
2023-02-06 13:35:14 -0300

    use while loop

--- tutorial/src/tutorial/Main.java
+++ tutorial/src/tutorial/Main.java
@@ -5,18 +5,14 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int[] arr = { 1, 1, 2, 3, 5, 8, 13 };
-		String[] names = new String[5];
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Type a number: ");
+		int x = sc.nextInt();
 
-		for (int i = 0; i < names.length; i++) {
-			System.out.print("Input: ");
-			String nameInput = sc.nextLine();
-			names[i] = nameInput;
-		}
-
-		for (String name : names) {
-			System.out.println(name);
+		while (x != 10) {
+			System.out.println("Type 10...");
+			System.out.print("Type a number: ");
+			x = sc.nextInt();
 		}
 	}
 
