10ba8d643f49facca80f1e4d8e7db56110ec2dbb
Tiago Gomes <tidgomes@gmail.com>
2023-02-06 13:52:26 -0300

    create a set

--- tutorial/src/tutorial/Main.java
+++ tutorial/src/tutorial/Main.java
@@ -1,18 +1,33 @@
 package tutorial;
 
-import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Type a number: ");
-		int x;
+		// A set cannot contain the same element twice
+		Set<Integer> t = new HashSet<Integer>();
+		t.add(1);
+		t.add(1);
+		t.add(2);
+		t.add(3);
+		t.add(5);
+		t.add(8);
+		t.add(9);
 
-		do {
-			System.out.print("Type a number: ");
-			x = sc.nextInt();
-		} while (x != 10);
+		System.out.println(t);
+		System.out.printf("The size of the set t is: %d%n", t.size());
+		System.out.println(t.contains(5));
+		System.out.println(t.contains(4));
+
+		t.remove(9);
+		System.out.println(t);
+
+		t.clear();
+		System.out.println(t);
+
+		System.out.println(t.isEmpty());
+		System.out.printf("The size of the set t now is: %d%n", t.size());
 	}
-
 }
