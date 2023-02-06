98515e16d3778b2cef9b0acd8efadb39925170ab
Tiago Gomes <tidgomes@gmail.com>
2023-02-06 13:37:10 -0300

    use do while loop

--- tutorial/src/tutorial/Main.java
+++ tutorial/src/tutorial/Main.java
@@ -7,13 +7,12 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Type a number: ");
-		int x = sc.nextInt();
+		int x;
 
-		while (x != 10) {
-			System.out.println("Type 10...");
+		do {
 			System.out.print("Type a number: ");
 			x = sc.nextInt();
-		}
+		} while (x != 10);
 	}
 
 }
