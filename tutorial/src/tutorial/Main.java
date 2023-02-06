4954ff058967e79777e89498fd716f559b903862
Tiago Gomes <tidgomes@gmail.com>
2023-02-06 12:17:13 -0300

    create primitive values

--- tutorial/src/tutorial/Main.java
+++ tutorial/src/tutorial/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		int number = 5;
+		double number2 = 5.0;
+		boolean b = true;
+		char c = 't';
+		String str = "Tiago";
+		System.out.println(str);
 
 	}
 
