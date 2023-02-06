1575f013e7aa09f626db3e84eb8b8bcdc3a2064f
Tiago Gomes <tidgomes@gmail.com>
2023-02-06 14:13:35 -0300

    create LinkedHashMap

--- tutorial/src/tutorial/Main.java
+++ tutorial/src/tutorial/Main.java
@@ -1,15 +1,16 @@
 package tutorial;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TreeMaps must have the same type for the keys
-		Map m = new TreeMap();
+		// LinkedHashMaps care about the order
+		// Doesn't care if the types of the keys are the same
+		Map m = new LinkedHashMap<>();
 		m.put("mano", "top G");
-		m.put("truta", 2);
+		m.put(3, 2);
 		m.put("tiu", 3);
 		m.put("fulano", "putinha");
 
