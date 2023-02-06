5315c9b17299c2454d63efdf4276093155701482
Tiago Gomes <tidgomes@gmail.com>
2023-02-06 14:19:44 -0300

    test Map basic functions

--- tutorial/src/tutorial/Main.java
+++ tutorial/src/tutorial/Main.java
@@ -1,19 +1,25 @@
 package tutorial;
 
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// LinkedHashMaps care about the order
-		// Doesn't care if the types of the keys are the same
-		Map m = new LinkedHashMap<>();
-		m.put("mano", "top G");
-		m.put(3, 2);
-		m.put("tiu", 3);
-		m.put("fulano", "putinha");
+		Map starman = new HashMap();
+		starman.put("name", "Starman Top G");
+		starman.put("age", 123);
+		starman.put("address", "The Moon");
+		starman.put("id", 123412);
 
-		System.out.println(m);
+		System.out.println(starman.containsKey(123));
+		System.out.println(starman.containsValue(123));
+
+		System.out.println(starman);
+		System.out.println(starman.isEmpty());
+
+		starman.clear();
+		System.out.println(starman);
+		System.out.println(starman.isEmpty());
 	}
 }
