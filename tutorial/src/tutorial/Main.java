ebdfc7401d9a68287ce09348c635aef86f3b2eda
Tiago Gomes <tidgomes@gmail.com>
2023-02-06 12:38:24 -0300

    create Scanner

--- tutorial/src/tutorial/Main.java
+++ tutorial/src/tutorial/Main.java
@@ -1,16 +1,15 @@
 package tutorial;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int number = 5;
-		double number2 = 5.0;
-		boolean b = true;
-		char c = 't';
-		String str = "Tiago";
-		System.out.println(str);
-
+		Scanner sc = new Scanner(System.in);
+		String scanned = sc.next();
+		
+		System.out.println(scanned);
 	}
 
 }
