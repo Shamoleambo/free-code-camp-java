ba68a8c829e5a76d39962916ad901a7ef2901a77
Tiago Gomes <tidgomes@gmail.com>
2023-02-06 13:29:50 -0300

    use for and forEach loops

--- tutorial/src/tutorial/Main.java
+++ tutorial/src/tutorial/Main.java
@@ -1,15 +1,23 @@
 package tutorial;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		int[] arr = { 1, 1, 2, 3, 5, 8, 13 };
 		String[] names = new String[5];
+		Scanner sc = new Scanner(System.in);
 
-		for (int element : arr) {
-			System.out.println(element);
+		for (int i = 0; i < names.length; i++) {
+			System.out.print("Input: ");
+			String nameInput = sc.nextLine();
+			names[i] = nameInput;
 		}
-		
+
+		for (String name : names) {
+			System.out.println(name);
+		}
 	}
 
 }
