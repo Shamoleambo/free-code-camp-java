4ad2e4cfe37bbcc53b839d833ef0913a2a7f25c2
Tiago Gomes <tidgomes@gmail.com>
2023-02-06 13:08:15 -0300

    create loop without tracking variable

--- tutorial/src/tutorial/Main.java
+++ tutorial/src/tutorial/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		int[] arr = { 1, 1, 2, 3, 5, 8, 13 };
-		for (int i = 0; i < arr.length; i++) {
-			System.out.printf("Position %d: %d%n", i, arr[i]);
+		String[] names = new String[5];
+
+		for (int element : arr) {
+			System.out.println(element);
 		}
+		
 	}
 
 }
