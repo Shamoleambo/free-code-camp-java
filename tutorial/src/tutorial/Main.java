71ce1c8fbe1bc37bf61d00f84aeda8cc3deb9a75
Tiago Gomes <tidgomes@gmail.com>
2023-02-06 14:11:36 -0300

    create TreeMap

--- tutorial/src/tutorial/Main.java
+++ tutorial/src/tutorial/Main.java
@@ -1,15 +1,13 @@
 package tutorial;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// HashMap doesn't retain the order;
-		// Cannot have two of the same keys, but you can have two of the same values;
-		// It is a very fast data set because doesn't care about order
-		Map m = new HashMap();
+		// TreeMaps must have the same type for the keys
+		Map m = new TreeMap();
 		m.put("mano", "top G");
 		m.put("truta", 2);
 		m.put("tiu", 3);
