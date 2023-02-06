c5e3fe5d3b1f1eb651b9a0d58ec6d9c052ab8443
Tiago Gomes <tidgomes@gmail.com>
2023-02-06 14:27:10 -0300

    sort a regular array

--- tutorial/src/tutorial/Main.java
+++ tutorial/src/tutorial/Main.java
@@ -1,25 +1,17 @@
 package tutorial;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Map starman = new HashMap();
-		starman.put("name", "Starman Top G");
-		starman.put("age", 123);
-		starman.put("address", "The Moon");
-		starman.put("id", 123412);
+		int[] arr = { 8, 2, 3, 1, 1, 5 };
 
-		System.out.println(starman.containsKey(123));
-		System.out.println(starman.containsValue(123));
+		// The last argument is non inclusive
+		Arrays.sort(arr, 0, arr.length);
 
-		System.out.println(starman);
-		System.out.println(starman.isEmpty());
-
-		starman.clear();
-		System.out.println(starman);
-		System.out.println(starman.isEmpty());
+		for (int element : arr) {
+			System.out.println(element);
+		}
 	}
 }
