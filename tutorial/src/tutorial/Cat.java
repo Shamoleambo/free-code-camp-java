96f1f4ddcd70847bcb6bccf44c315b7606bba225
Tiago Gomes <tidgomes@gmail.com>
2023-02-07 10:26:05 -0300

    create getter and setter

--- tutorial/src/tutorial/Cat.java
+++ tutorial/src/tutorial/Cat.java
@@ -4,13 +4,21 @@ public class Cat {
 
 	private String name;
 	private int age;
-	
+
 	public Cat(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
-	
+
 	public void speak() {
 		System.out.printf("I am %s and I am %d years old%n", this.name, this.age);
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 }
--- tutorial/src/tutorial/Main.java
+++ tutorial/src/tutorial/Main.java
@@ -5,8 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		Cat maninho = new Cat("maninho", 4);
 		maninho.speak();
-		
+
 		Cat arrois = new Cat("arrois", 7);
 		arrois.speak();
+
+		System.out.println(maninho.getAge());
+		maninho.setAge(5);
+		System.out.println(maninho.getAge());
 	}
 }
